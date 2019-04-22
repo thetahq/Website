b769081ffb8c0232d19ff10dbeecb6c3beca08fa
Mikolaj Radkowski <yknomeh@protonmail.com>
2019-04-22 17:42:19 +0200

    dashboard panel

--- src/components/authorized/DashboardComponent.js
+++ src/components/authorized/DashboardComponent.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { Box, Heading, ResponsiveContext, Grid, Button } from 'grommet';
+import { Box, Heading, ResponsiveContext, Grid, Button, Text } from 'grommet';
+import { Ubuntu, Archlinux, Centos, Fedora } from 'grommet-icons';
 import ContainerSelector from '../widgets/SelectContainerComponent';
+import Panel from '../widgets/DashboardPanelComponent';
 
 class DashboardComponent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selected: '',
+      selectedContainer: '',
+      selected: 'main',
       containers: [
         {
           id: 1,
@@ -33,47 +36,127 @@ class DashboardComponent extends Component {
     }
   }
 
+  getIcon() {
+    switch (this.props.osName) {
+      case 'ubuntu':
+      return <Ubuntu color='#FF8F00' />
+      case 'arch':
+      return <Archlinux color='#1AE3E0' />
+      case 'centos':
+      return <Centos color='#881AE3' />
+      case 'fedora':
+      return <Fedora color='#000EE3' />
+      default:
+      return
+    }
+  }
+
   render() {
+    const menuHandler = (selected) => {
+      this.setState({
+        selected: selected
+      });
+
+      if (selected === '') {
+        this.setState({
+          selectedContainer: ''
+        });
+      }
+    }
+
+    const mainMenu = (
+      <Box
+        animation='fadeIn'
+        fill={true}
+        background='black'
+        align='center'
+        margin={{
+          left: '2rem'
+        }}
+      >
+        <Heading>{this.state.selectedContainer.name}</Heading>
+        <Text>Operating system: {this.state.selectedContainer.os}</Text>
+      </Box>
+    );
+
+    const menuRenderer = (
+      <div>
+        {this.state.selected === 'main' ? mainMenu : <div></div>}
+        {this.state.selected === 'resources' ? <Heading>resources</Heading> : <div></div>}
+        {this.state.selected === 'terminal' ? <Heading>terminal</Heading> : <div></div>}
+        {this.state.selected === 'tasks' ? <Heading>tasks</Heading> : <div></div>}
+        {this.state.selected === 'shop' ? <Heading>shop</Heading> : <div></div>}
+        {this.state.selected === 'settings' ? <Heading>settings</Heading> : <div></div>}
+      </div>
+    );
+
+    const selecter = (
+      <Box
+        animation='fadeIn'
+        align='center'
+        justify='center'
+        fill='horizontal'
+        pad='medium'
+        background='black'
+      >
+        <Heading level='1' size='medium'>Select your container</Heading>
+        <Grid
+          rows={['xsmall', 'xsmall']}
+          columns={['small', 'small', 'small']}
+          justify='center'
+          gap='medium'
+          areas={[
+            { name: 'con1', start: [0, 0], end: [0, 0] },
+            { name: 'con2', start: [1, 0], end: [1, 0] },
+            { name: 'con3', start: [2, 0], end: [2, 0] },
+            { name: 'con4', start: [0, 1], end: [0, 1] },
+            { name: 'con5', start: [1, 1], end: [1, 1] },
+            { name: 'con6', start: [2, 1], end: [2, 1] },
+          ]}
+        >
+          {this.state.containers.map(cont => (
+            <ContainerSelector
+              key={cont.id}
+              contId={cont.id}
+              osName={cont.os}
+              contName={cont.name}
+              Selected={e => {
+                this.setState({
+                  selectedContainer: cont,
+                  selected: 'main'
+                })
+              }
+              }
+            />)
+          )
+          }
+        </Grid>
+        <Button margin={{ top: '2rem' }} color='brand' primary={true} label='Create new container' />
+      </Box>
+    );
+
     return (
       <ResponsiveContext.Consumer>
-        {() => (
-          <Box
-            animation='fadeIn'
-            align='center'
-            justify='center'
-            fill='horizontal'
-            pad='medium'
-            background='black'
-          >
-            <Heading level='1' size='medium'>Select your container</Heading>
-            <Grid
-              rows={['xsmall', 'xsmall']}
-              columns={['small', 'small', 'small']}
-              justify='center'
-              gap='medium'
-              areas={[
-                { name: 'con1', start: [0, 0], end: [0, 0] },
-                { name: 'con2', start: [1, 0], end: [1, 0] },
-                { name: 'con3', start: [2, 0], end: [2, 0] },
-                { name: 'con4', start: [0, 1], end: [0, 1] },
-                { name: 'con5', start: [1, 1], end: [1, 1] },
-                { name: 'con6', start: [2, 1], end: [2, 1] },
-              ]}
-            >
-            {this.state.containers.map(cont => (
-              <ContainerSelector
-                key={cont.id}
-                contId={cont.id}
-                osName={cont.os}
-                contName={cont.name}
-                Selected={e => this.setState({selected: e})} 
-              />)
-              )
-            }
-            </Grid>
-            <Button margin={{top: '2rem'}} color='brand' primary={true} label='Create new container' />
-          </Box>
-        )}
+        {() => {
+          return this.state.selectedContainer !== '' ?
+            (
+              <Box
+                direction='row'
+                fill={true}
+              >
+
+                <Panel Selected={(e) => menuHandler(e)} />
+                { menuRenderer }
+              </Box>
+            )
+
+            : (
+              <div>
+                {selecter}
+              </div>
+            )
+        }
+        }
       </ResponsiveContext.Consumer>
     );
   }
--- src/components/widgets/DashboardPanelComponent.js
+++ src/components/widgets/DashboardPanelComponent.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import { Box, ResponsiveContext, Text } from 'grommet';
-import { Ubuntu, Archlinux, Centos, Fedora } from 'grommet-icons';
+import { ServerCluster, Resources, FormSchedule, Currency, Configure, Terminal, FormPreviousLink } from 'grommet-icons';
 
 class DashboardPanelComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 'main'
+    }
+
+    this.setSelected = this.setSelected.bind(this);
+  }
+
+  setSelected(selected) {
+    this.setState({
+      selected: selected
+    });
+
+    this.props.Selected(selected);
+  }
 
   render() {
     return (
       <ResponsiveContext.Consumer>
         {() => (
-          <Grid
-            rows={['xsmall', 'xsmall']}
-            columns={['small', 'small', 'small']}
-            justify='center'
+          <Box
+            animation='fadeIn'
+            fill='vertical'
+            background='black'
+            pad='medium'
             gap='medium'
-            areas={[
-              { name: 'title', start: [0, 0], end: [0, 0] },
-              { name: 'body', start: [1, 0], end: [1, 0] }
-            ]}
           >
-
-          </Grid>
+            <FormPreviousLink color={this.state.selected === '' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('')} />
+            <ServerCluster color={this.state.selected === 'main' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('main')} />
+            <Resources color={this.state.selected === 'resources' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('resources')} />
+            <Terminal color={this.state.selected === 'terminal' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('terminal')} />
+            <FormSchedule color={this.state.selected === 'tasks' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('tasks')} />
+            <Currency color={this.state.selected === 'shop' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('shop')} />
+            <Configure color={this.state.selected === 'settings' ? 'brand' : 'white'} style={{cursor: "pointer"}} onClick={() => this.setSelected('settings')} />
+          </Box>
         )}
       </ResponsiveContext.Consumer>
     );
